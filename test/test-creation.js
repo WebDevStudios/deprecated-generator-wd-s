288f3e65c99edbd56dce70af870515faa347bcd3
Camden Segal <camden.segal@gmail.com>
2014-06-23 09:10:07 -0400

    Some cleanup

--- test/test-creation.js
+++ test/test-creation.js
@@ -3,7 +3,7 @@
 var path = require('path');
 var helpers = require('yeoman-generator').test;
 
-describe('wd_s generator', function () {
+describe('wd_s', function () {
   beforeEach(function (done) {
     helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
       if (err) {
--- test/test-load.js
+++ test/test-load.js
@@ -2,7 +2,7 @@
 'use strict';
 var assert = require('assert');
 
-describe('wd_s generator', function () {
+describe('wd_s', function () {
   it('can be imported without blowing up', function () {
     var app = require('../app');
     assert(app !== undefined);
