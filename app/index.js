f478e554f12434d6e20b155b7d057c4079700897
Camden Segal <camden.segal@gmail.com>
2015-04-10 16:03:27 -0400

    Rewrite working

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 'use strict';
 var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
-
+var chalk  = require('chalk');
+var yosay  = require('yosay');
+var git    = require('nodegit');
 module.exports = yeoman.generators.Base.extend({
   initializing: function () {
     this.pkg = require('../package.json');
@@ -13,50 +13,133 @@ module.exports = yeoman.generators.Base.extend({
 
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the grand ' + chalk.red('WdS') + ' generator!'
+      'Welcome to the killer ' + chalk.blue('WD_s') + ' generator!'
     ));
 
     var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
+      type: 'text',
+      name: 'name',
+      message: 'Project Title',
+      default: 'WDS Project Title'
+    },{
+      type: 'text',
+      name: 'shortname',
+      message: 'Slug',
+      default: function(props){
+        return this._.slugify( props.name );
+      }.bind(this)
+    },{
+      type: 'text',
+      name: 'uri',
+      message: 'Project URI',
+      default: 'http://webdevstudios.com'
+    },{
+      type: 'text',
+      name: 'author',
+      message: 'Author',
+      default: 'WebDevStudios',
+      save: true
+    },{
+      type: 'text',
+      name: 'authoruri',
+      message: 'Author URI',
+      default: 'http://webdevstudios.com',
+      save: true
+    },{
+      type: 'text',
+      name: 'description',
+      message: 'Description',
+      default: 'A new WD_s project!'
     }];
 
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
+  this.prompt(prompts, function (props) {
+      this.name = props.name;
+      this.slug = this._.slugify( props.slug );
+      this.uri  = props.uri;
+      this.author = props.author;
+      this.authoruri = props.authoruri;
+      this.description = props.description;
 
       done();
     }.bind(this));
   },
 
   writing: {
-    app: function () {
-      this.fs.copy(
-        this.templatePath('_package.json'),
-        this.destinationPath('package.json')
-      );
-      this.fs.copy(
-        this.templatePath('_bower.json'),
-        this.destinationPath('bower.json')
-      );
+    git: function() {
+      var done = this.async();
+      this.log('Starting Git repo in ' + this.destinationPath());
+
+      git.Repository.init( this.destinationPath(), 0 )
+        .then(function(repo){
+          this.log('Git repo initialized');
+          console.log( repo.path() );
+
+          this.repo = repo;
+
+          done();
+        }.bind(this));
     },
 
     projectfiles: function () {
+      this.log('Setting up project configurations');
       this.fs.copy(
-        this.templatePath('editorconfig'),
-        this.destinationPath('.editorconfig')
+        this.templatePath('_gitignore'),
+        this.destinationPath('.gitignore')
       );
-      this.fs.copy(
-        this.templatePath('jshintrc'),
-        this.destinationPath('.jshintrc')
+
+      this.fs.copyTpl(
+        this.templatePath('composer.json'),
+        this.destinationPath('composer.json'),
+        this
       );
+    },
+
+    muPlugins: function() {
+      var done = this.async();
+      this.log('Installing mu-plugins');
+
+      this.fs.copyTpl(
+        this.templatePath('mu-plugins/mu-plugins-include.php'),
+        this.destinationPath('mu-plugins/mu-plugins-include.php'),
+        this
+      );
+
+      var modules = [{
+            name: 'cmb2',
+            url:  'https://github.com/WebDevStudios/CMB2.git'
+          },{
+            name: 'cpt-core',
+            url:  'https://github.com/WebDevStudios/CPT_Core.git'
+          },{
+            name: 'taxonomy-core',
+            url:  'https://github.com/WebDevStudios/Taxonomy_Core.git'
+          },{
+            name: 'wds-required-plugins',
+            url:  'https://github.com/WebDevStudios/WDS-Required-Plugins.git'
+          }],
+          i = -1,
+          subRepo;
+
+      var nextModule = function() {
+        i += 1;
+        if ( i >= modules.length ) {
+          done();
+          return;
+        }
+        this.spawnCommand('git', ['submodule', 'add', modules[i].url, 'mu-plugins/' + modules[i].name ] ).on( 'close', nextModule );
+      }.bind(this);
+
+      nextModule();
+    },
+
+    theme: function() {
+      this.log('Installing theme');
     }
   },
 
   install: function () {
-    this.installDependencies({
-      skipInstall: this.options['skip-install']
-    });
+    if ( ! this.options['skip-install'] ) {
+      this.spawnCommand('composer', ['install'] );
+    }
   }
 });
---
+++ app/templates/mu-plugins/mu-plugins-include.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Include mu-plugins files.
+ */
+include_once('cmb2/init.php');
+include_once('cpt-core/CPT_Core.php');
+include_once('taxonomy-core/Taxonomy_Core.php');
+include_once('wds-required-plugins/wds-required-plugins.php');
